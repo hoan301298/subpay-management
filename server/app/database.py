b8390bb7ddccc2537606e2455d6aa61d344462f6
hoan301298 <hoantran301298@gmail.com>
2026-02-04 18:48:49 +0200

    setup database connection

---
+++ server/app/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("POSTGRE_URL")
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+SessionLocal = sessionmaker(
+    bind=engine, class_=AsyncEngine, expire_on_commit=False
+)
+
+Base = declarative_base()
+
+async def get_db():
+    async with SessionLocal() as session:
+        yield session
