06ccf1ee55a994ba8d122b5b6277f60def00b44a
hoan301298 <hoantran301298@gmail.com>
2026-02-04 19:07:28 +0200

    fix ssl asyncpg

--- server/app/database.py
+++ server/app/database.py
@@ -1,13 +1,22 @@
 from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
+import ssl
 from dotenv import load_dotenv
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("POSTGRE_URL")
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+ssl_context = ssl.create_default_context()
+ssl_context.check_hostname = False
+ssl_context.verify_mode = ssl.CERT_NONE
+
+engine = create_async_engine(
+    DATABASE_URL, 
+    echo=True,
+    connect_args={"ssl": ssl_context}
+)
 
 SessionLocal = sessionmaker(
     bind=engine, class_=AsyncEngine, expire_on_commit=False
